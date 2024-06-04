4e372ced482f644e7e2640e2f8fcb3f068853f5a
scrahe <derekneubert11@gmail.com>
2024-06-03 21:33:52 -0500

    Files Test (Succeeded)

---
+++ read2.es
@@ -0,0 +1,6 @@
+read.es
+hi
+bye
+hello!
+hello
+hello
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
+int main(int argc, char* argv[]) {
+  string line;
+  ifstream file(argv[1]);
+  while (getline(file, line)) {
+    if (line == "hello") {
+      cout << "Hello to you too!\n";
+    }
+  }
   return 0;
 }
