45e1fed0d14f5eb4bc496564cbc979a8a8ca563e
scrahe <derekneubert11@gmail.com>
2024-06-04 20:34:12 -0500

    Option checking

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,23 @@
 #include <fstream>
 using namespace std;
 
+void parseOptions(char* argv[], int argc) {
+  for (int i = 0; i < argc; ++i) {
+    if (argv[i][0] == '-') {
+      for (int j : argv[i]) {
+        if (j == 0) {
+          break
+        }
+        if (j == 1 and argv[i][j] == '-') {
+          cout << "literal"
+        }
+      }
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
+  parseOptions(argv, argc);
   string line;
   ifstream file(argv[1]);
   while (getline(file, line)) {
